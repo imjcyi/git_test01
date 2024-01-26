b056a9e45d076ec16ef4651b482e0049e9f4940c
shanfengg <3143755332@qq.com>
2024-01-26 17:03:27 +0800

    月度账单-单日账单显示/隐藏

--- src/page/Month/components/DayItem/index.js
+++ src/page/Month/components/DayItem/index.js
@@ -1,6 +1,6 @@
 import classNames from 'classnames'
 import './index.scss'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import { billTypeToName } from '@/contants/index'
 
 const DayItem = ({ date, billList }) => {
@@ -13,12 +13,14 @@ const DayItem = ({ date, billList }) => {
             total: pay + inCome
         }
     }, [billList])
+
+    const [visible, setVisible] = useState(false);
     return (
         <div className={classNames('dailyBill')}>
             <div className="header">
                 <div className="dateIcon">
                     <span className="date">{date}</span>
-                    <span className={classNames('arrow')}></span>
+                    <span className={classNames('arrow', visible && 'expand')} onClick={() => setVisible(!visible)}></span>
                 </div>
                 <div className="oneLineOverview">
                     <div className="pay">
@@ -36,7 +38,7 @@ const DayItem = ({ date, billList }) => {
                 </div>
             </div>
             {/* 单日列表 */}
-            <div className="billList">
+            <div className="billList" style={{ display: visible ? 'block' : 'none' }}>
                 {billList.map(item => {
                     return (
                         <div className="bill" key={item.id}>
