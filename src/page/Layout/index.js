4ac144b3647be50a07e1b232a79d3560f0c9933a
shanfengg <3143755332@qq.com>
2024-01-26 14:41:28 +0800

    主题色配置

--- src/page/Layout/index.js
+++ src/page/Layout/index.js
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
-
+import { Button } from "antd-mobile";
 const Layout = () => {
     return (
         <div>
             Layout
             <Outlet />
+            <Button color="primary">测试</Button>
         </div>
     )
 }
